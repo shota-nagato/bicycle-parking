3dc70c81e2b1ad78347b7a4fe171b36b5f5aa1da
shota-nagato <shota.nagato@gmail.com>
2023-12-29 19:24:14 +0900

    with_optionsでまとめる

--- app/models/bicycle_parking.rb
+++ app/models/bicycle_parking.rb
@@ -1,15 +1,19 @@
 class BicycleParking < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
-  validates :location, presence: true
-  validates :opening_hours, presence: true
-  validates :bicycle_capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :motorized_bicycle_under_50cc_capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :motorized_bicycle_under_125cc_capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :bicycle_and_motorized_bicycle_under_50cc_capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :motorcycle_capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :motorcycle_price, presence: true, if: :motorcycle_parkable?
-  validates :latitude, presence: true
-  validates :longitude, presence: true
+  with_options presence: true do
+    validates :name, uniqueness: true
+    validates :location
+    validates :opening_hours
+    validates :motorcycle_price, if: :motorcycle_parkable?
+    validates :latitude
+    validates :longitude
+    with_options numericality: { only_integer: true, greater_than_or_equal_to: 0 } do
+      validates :bicycle_capacity
+      validates :motorized_bicycle_under_50cc_capacity
+      validates :motorized_bicycle_under_125cc_capacity
+      validates :bicycle_and_motorized_bicycle_under_50cc_capacity
+      validates :motorcycle_capacity
+    end
+  end
 
   def motorcycle_parkable?
     motorcycle_capacity > 0
