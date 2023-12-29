e310135262a4b884f4ca864c306ee0857f8b5487
shota-nagato <shota.nagato@gmail.com>
2023-12-29 22:52:52 +0900

    seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "csv"
+
+CSV.foreach("db/csv/2023_03_31_bicycle_parking_list.csv", headers: true, encoding: "utf-8") do |row|
+  bicycle_capacity = row["自転車収容台数"] == "-" ? 0 : row["自転車収容台数"].to_i
+  motorized_bicycle_under_50cc_capacity = row["50cc以下の原動機付自転車収容台数"] == "-" ? 0 : row["50cc以下の原動機付自転車収容台数"].to_i
+  motorized_bicycle_under_125cc_capacity = row["125cc以下の原動機付自転車収容台数"] == "-" ? 0 : row["125cc以下の原動機付自転車収容台数"].to_i
+  bicycle_and_motorized_bicycle_under_50cc_capacity = row["自転車・50cc以下の原動機付自転車収容台数"] == "-" ? 0 :row["自転車・50cc以下の原動機付自転車収容台数"].to_i
+  motorcycle_price = row["自動二輪車料金"] == "-" ? "" : row["自動二輪車料金"]
+  bicycle_parking = BicycleParking.create!(
+    name: row["名称"],
+    location: row["所在地"],
+    opening_hours: row["開場時間"],
+    bicycle_capacity: bicycle_capacity,
+    motorized_bicycle_under_50cc_capacity: motorized_bicycle_under_50cc_capacity,
+    motorized_bicycle_under_125cc_capacity: motorized_bicycle_under_125cc_capacity,
+    bicycle_and_motorized_bicycle_under_50cc_capacity: bicycle_and_motorized_bicycle_under_50cc_capacity,
+    motorcycle_capacity: row["自動二輪車収容台数"].to_i,
+    motorcycle_price: motorcycle_price,
+    latitude: row["緯度"],
+    longitude: row["経度"],
+    published: true
+  )
+end
