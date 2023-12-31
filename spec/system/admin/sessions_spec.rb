9fdbbddc47ee9e5265220c04416113c5e6b83997
shota-nagato <shota.nagato@gmail.com>
2023-12-31 23:04:34 +0900

    管理者ログインのテスト追加

---
+++ spec/system/admin/sessions_spec.rb
@@ -0,0 +1,54 @@
+require "rails_helper"
+
+RSpec.describe "管理者機能", type: :system do
+  let!(:admin) { create(:admin) }
+
+  describe 'ログイン機能' do
+    before do
+      visit new_admin_session_path
+    end
+
+    context '正しいメールアドレスとパスワードを入力した場合' do
+      it 'ログインに成功し、管理者ページに遷移する' do
+        fill_in 'メールアドレス', with: 'admin@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログイン'
+        expect(page).to have_current_path admin_root_path
+        expect(page).to have_content 'ログインしました'
+        expect(page).to have_content 'admin@example.com'
+      end
+    end
+
+    context '誤ったメールアドレスとパスワードを入力した場合' do
+      it 'ログインに失敗し、ページ遷移しない' do
+        fill_in 'メールアドレス', with: 'invalid@example.com'
+        fill_in 'パスワード', with: 'invalid password'
+        click_button 'ログイン'
+        expect(page).to have_current_path new_admin_session_path
+        expect(page).to have_content 'メールアドレスまたはパスワードが違います。'
+      end
+    end
+
+    context 'ログインしていない状態で管理ページにアクセスした場合' do
+      it 'ログインページに遷移する' do
+        visit admin_root_path
+        expect(page).to have_current_path new_admin_session_path
+        expect(page).to have_content 'ログインもしくはアカウント登録してください'
+      end
+    end
+  end
+
+  describe 'ログアウト機能' do
+    context 'ログインした状態でログアウトボタンを押した場合' do
+      it 'ログインページに遷移する' do
+        sign_in admin
+        visit admin_root_path
+        click_on 'admin@example.com'
+        click_button 'ログアウト'
+        expect(page).to have_current_path new_admin_session_path
+        expect(page).to have_content 'ログアウトしました'
+        expect(page).not_to have_button 'ログアウト'
+      end
+    end
+  end
+end
