9108183a27314cfb1d3e53f795a13ed01daf6d1e
shota-nagato <shota.nagato@gmail.com>
2024-01-03 18:50:22 +0900

    車種での絞り込み機能

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,16 +1,32 @@
 class MapController < ApplicationController
+  before_action :set_japan_style_url
+  before_action :set_search_params
+
   has_scope :motorized_bicycle_under_50cc_parkable, type: :boolean
   has_scope :motorized_bicycle_under_125cc_parkable, type: :boolean
   has_scope :motorcycle_parkable, type: :boolean
 
   def index
-    @mapbox_japan_style = ENV["MAPBOX_JAPAN_STYLE"]
     bicycle_parkings = apply_scopes(BicycleParking).all
     if bicycle_parkings.count > 0
-      flash[:notice] = "#{bicycle_parkings.count}件見つかりました"
+      flash.now[:notice] = "#{bicycle_parkings.count}件見つかりました"
     else
-      flash[:alert] = '見つかりませんでした'
+      flash.now[:alert] = '見つかりませんでした'
     end
     @data = bicycle_parkings.map { |parking| { latitude: parking.latitude, longitude: parking.longitude, tooltip: render_to_string(partial: 'map/info', locals: { parking: parking }) } }
+  end
+
+  private
+
+  def set_japan_style_url
+    @mapbox_japan_style = ENV["MAPBOX_JAPAN_STYLE"]
+  end
+
+  def set_search_params
+    session[:search_params] = search_params
+  end
+
+  def search_params
+    params.permit(:motorized_bicycle_under_50cc_parkable, :motorized_bicycle_under_125cc_parkable, :motorcycle_parkable)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
     root to: "bicycle_parkings#index"
   end
   root "map#index"
+  resources :search, only: :index
 end
