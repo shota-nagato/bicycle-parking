7c0dbe18e7e2969c1ef5360904e8f475b199a3e8
shota-nagato <shota.nagato@gmail.com>
2024-04-13 12:56:57 +0900

    駐輪場を編集できるようにする

--- app/controllers/admin/bicycle_parkings_controller.rb
+++ app/controllers/admin/bicycle_parkings_controller.rb
@@ -1,26 +1,47 @@
 class Admin::BicycleParkingsController < Admin::ApplicationController
+  before_action :set_bicycle_parking, only: %i[show edit update destroy]
+
   def index
     @bicycle_parkings = BicycleParking.default_order
+  end
+
+  def show
   end
 
   def new
     @bicycle_parking = BicycleParking.new
   end
 
+  def edit
+  end
+
   def create
     @bicycle_parking = BicycleParking.new(bicycle_parking_params)
     if @bicycle_parking.save
-      redirect_to admin_bicycle_parkings_path
+      redirect_to admin_bicycle_parkings_path, notice: "作成しました"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def show
-    @bicycle_parking = BicycleParking.find(params[:id])
+  def update
+    if @bicycle_parking.update(bicycle_parking_params)
+      redirect_to admin_bicycle_parkings_path, notice: "更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bicycle_capacity.destroy!
+    redirect_to admin_bicycle_parkings_path, notice: "削除しました"
   end
 
   private
+
+  def set_bicycle_parking
+    @bicycle_parking = BicycleParking.find(params[:id])
+  end
 
   def bicycle_parking_params
     params.require(:bicycle_parking).permit(
